086ad2a349a96d1dd321d4b4748a2ae43e20391e
geison-bkz <geison_bf@hotmail.com>
2024-08-10 18:40:06 -0300

    AutomatosFinitosBackend

---
+++ AutomatosFinitosBackend/src/test/java/com/automatosfinitos/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automatosfinitos.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
