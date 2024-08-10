f1875f8655e5795ba255527b7c7161dbd589f5f6
geison-bkz <geison_bf@hotmail.com>
2024-08-10 18:35:35 -0300

    MelhorRota

---
+++ MelhorRota/src/test/java/com/springboot/a3melhorrota/A3MelhorRotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.a3melhorrota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A3MelhorRotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
